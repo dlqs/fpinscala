949409d412973dece5a6189b63273d18c885abdf
Donald L <dlqs@gmx.com>
2021-01-06 15:38:46 +0800

    4.2

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -58,6 +58,7 @@ object Option {
     println(t.orElse(Some(4)))
     println(s.getOrElse(4))
     println(t.getOrElse(4))
+    println(variance(Seq(600, 470, 170, 430, 300))) // 21704
   }
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
@@ -79,7 +80,9 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap ((m) => mean(xs map ((x) => math.pow(x - m, 2))))
+  }
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
