f2c7e667b7382a81e274c92a01cf3771e1183740
Donald Lee <dlqs@gmx.com>
2021-01-21 12:18:36 +0800

    9.8

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,68 @@
 package fpinscala.parsing
 
 import language.higherKinds
+import scala.util.matching.Regex
+import fpinscala.testing._
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+  implicit def regex(r: Regex): Parser[String]
+
+  // reptitions of A, possibly none
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _) or succeed(List())
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+    flatMap(p)(a => succeed(f(a)))
+
+  def char(c: Char): Parser[Char] = 
+    string(c.toString) map (_.charAt(0))
+
+  // similar to unit from previous chapters
+  def succeed[A](a: A): Parser[A] = 
+    string("").map(_ => a)
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = {
+    if (n <= 0) succeed(List())
+    else map2(p, listOfN(n - 1, p))(_ :: _)
+  }
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    flatMap(p)(a => p2 map (b => (a, b)))
+
+  def flatten[A](p: Parser[Parser[A]]): Parser[A]
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      flatMap(p)(a => map(p2)(b => f(a, b)))
+
+  // reptitions of A, at least one
+  def many1[A](p: Parser[A]): Parser[List[A]] = 
+    map2(p, many(p))(_ ::_)
+
   case class ParserOps[A](p: Parser[A]) {
-
-
+    def | [B>:A](p2: Parser[A]): Parser[B] = self.or(p, p2)
+    def or [B>:A](p2: Parser[A]): Parser[B] = self.or(p, p2)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def ** [B>:A](p2: Parser[B]) = self.product(p, p2)
+    def product[B>:A](p2: Parser[B]) = self.product(p, p2)
+    def flatMap[B](f: A => Parser[B]) = self.flatMap(p)(f)
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
   }
 }
 
